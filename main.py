c07d2bb95b3b233756f231a05d6ed1b4c174cece
metalune <metalune@mailbox.org>
2021-07-18 21:30:32 +0200

    Ignore engine select field if there is only one engine

--- main.py
+++ main.py
@@ -139,6 +139,13 @@ async def index():
 
     use_text_fields = request.args.get("typingiscool") == "True"
 
+
+    ui_engines = [engine.name for engine in engines]
+
+    # if there is only one engine, set the ui_engines list to None, then the UI will ignore the entire engines select field
+    if len(ui_engines) == 1:
+        ui_engines = None
+
     return await render_template(
         "index.html",
         inp=inp,
@@ -148,7 +155,7 @@ async def index():
         to_l=to_lang,
         to_l_code=to_l_code,
         engine=engine.name,
-        engines=[engine.name for engine in engines],
+        engines=ui_engines,
         supported_languages=engine.get_supported_languages(),
         use_text_fields=use_text_fields,
     )
