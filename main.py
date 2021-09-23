524b1bf493914e2e9c9c0636784986682c971488
metalune <metalune@mailbox.org>
2021-09-23 15:30:32 +0200

    Run main.py through black

--- main.py
+++ main.py
@@ -10,33 +10,35 @@ import requests
 
 config = ConfigParser()
 
-config.read(['/etc/simplytranslate/shared.conf', '/etc/simplytranslate/web.conf'])
+config.read(["/etc/simplytranslate/shared.conf", "/etc/simplytranslate/web.conf"])
 
 engines = []
 
-if config.getboolean('google', 'Enabled', fallback=True):
+if config.getboolean("google", "Enabled", fallback=True):
     engines.append(GoogleTranslateEngine())
 
-libretranslate_enabled = config.getboolean('libretranslate', 'Enabled', fallback=None)
+libretranslate_enabled = config.getboolean("libretranslate", "Enabled", fallback=None)
 
 if libretranslate_enabled is None:
-    print("LibreTranslate is disabled by default; please edit the config file to explicitly state whether it is enabled or not")
+    print(
+        "LibreTranslate is disabled by default; please edit the config file to explicitly state whether it is enabled or not"
+    )
 
 if libretranslate_enabled:
     engines.append(
         LibreTranslateEngine(
-            config['libretranslate']['Instance'],
+            config["libretranslate"]["Instance"],
             # `ApiKey` is not required, so use `get` to get `None` as fallback.
-            config['libretranslate'].get('ApiKey'),
+            config["libretranslate"].get("ApiKey"),
         )
     )
 
 if not engines:
-    raise Exception('All translation engines are disabled')
+    raise Exception("All translation engines are disabled")
 
 app = Quart(__name__)
 
-#NOTE: Legacy Endpoint. Use "/api"
+# NOTE: Legacy Endpoint. Use "/api"
 @app.route(
     "/translate/<string:from_language>/<string:to_language>/<string:input_text>/"
 )
@@ -44,6 +46,7 @@ async def translate(from_language, to_language, input_text):
     return engines[0].translate(
         input_text, from_language=from_language, to_language=to_language
     )
+
 
 @app.route("/api/translate/")
 async def api_translate():
@@ -57,10 +60,8 @@ async def api_translate():
     from_language = to_lang_code(from_language, engine)
     to_language = to_lang_code(to_language, engine)
 
+    return engine.translate(text, from_language=from_language, to_language=to_language)
 
-    return engine.translate(
-        text, from_language=from_language, to_language=to_language
-    )
 
 @app.route("/api/get_languages/")
 async def api_get_languages():
@@ -68,6 +69,7 @@ async def api_get_languages():
     engine = get_engine(engine_name, engines, engines[0])
 
     return engine.get_languages()
+
 
 @app.route("/api/tts/")
 async def api_tts():
@@ -82,16 +84,15 @@ async def api_tts():
     url = engine.get_tts(text, language)
 
     USER_AGENT = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
-    
+
     if url != None:
         print(url)
-        return requests.get(url, headers={
-            "Referrer": None,
-            "User-Agent": USER_AGENT
-            }).content
+        return requests.get(
+            url, headers={"Referrer": None, "User-Agent": USER_AGENT}
+        ).content
     else:
         return None
-    
+
 
 @app.route("/switchlanguages/", methods=["POST"])
 async def switchlanguages():
@@ -127,12 +128,12 @@ async def switchlanguages():
     """
 
     redirect_params = {
-        'engine': engine_name,
-        'typingiscool': use_text_fields,
-        'sl': from_lang,
-        'tl': to_lang,
-        'text': text,
-        'could_not_switch_languages': from_lang == "auto",
+        "engine": engine_name,
+        "typingiscool": use_text_fields,
+        "sl": from_lang,
+        "tl": to_lang,
+        "text": text,
+        "could_not_switch_languages": from_lang == "auto",
     }
 
     response = await make_response(
@@ -142,10 +143,11 @@ async def switchlanguages():
         ),
     )
 
-    response.set_cookie('from_lang', to_lang)
-    response.set_cookie('to_lang', from_lang)
+    response.set_cookie("from_lang", to_lang)
+    response.set_cookie("to_lang", from_lang)
 
     return response
+
 
 @app.route("/typingiscool/", methods=["POST"])
 async def typingiscool():
@@ -163,17 +165,18 @@ async def typingiscool():
     use_text_fields = not use_text_fields
 
     redirect_params = {
-        'engine': engine_name,
-        'typingiscool': use_text_fields,
-        'sl': from_lang,
-        'tl': to_lang,
-        'text': text
+        "engine": engine_name,
+        "typingiscool": use_text_fields,
+        "sl": from_lang,
+        "tl": to_lang,
+        "text": text,
     }
 
     return redirect(
         f"/?{urlencode(redirect_params)}",
         code=302,
     )
+
 
 @app.route("/", methods=["GET", "POST"])
 async def index():
@@ -191,11 +194,17 @@ async def index():
         # support google format
         inp = request.args.get("text", "")
 
-        from_lang = to_full_name(request.args.get("sl") or request.cookies.get('from_lang') or "auto", engine)
+        from_lang = to_full_name(
+            request.args.get("sl") or request.cookies.get("from_lang") or "auto", engine
+        )
 
-        to_lang = to_full_name(request.args.get("tl") or request.cookies.get('to_lang') or "en", engine)
+        to_lang = to_full_name(
+            request.args.get("tl") or request.cookies.get("to_lang") or "en", engine
+        )
 
-        could_not_switch_languages = request.args.get("could_not_switch_languages") == "True"
+        could_not_switch_languages = (
+            request.args.get("could_not_switch_languages") == "True"
+        )
     elif request.method == "POST":
         form = await request.form
 
@@ -225,41 +234,35 @@ async def index():
     # check if the engine even supports TTS
     if engine.get_tts("auto", "test") != None:
         if len(inp) > 0:
-            params = {
-                "engine": engine_name,
-                "lang": from_l_code,
-                "text": inp
-            }
+            params = {"engine": engine_name, "lang": from_l_code, "text": inp}
             tts_from = f"/api/tts?{urlencode(params)}"
         if translation != None:
             if len(translation) > 0:
-                params = {
-                    "engine": engine_name,
-                    "lang": to_l_code,
-                    "text": translation
-                }
+                params = {"engine": engine_name, "lang": to_l_code, "text": translation}
                 tts_to = f"/api/tts?{urlencode(params)}"
 
-    response = await make_response(await render_template(
-        "index.html",
-        inp=inp,
-        translation=translation,
-        from_l=from_lang,
-        from_l_code=from_l_code,
-        tts_from=tts_from,
-        tts_to=tts_to,
-        to_l=to_lang,
-        to_l_code=to_l_code,
-        engine=engine.name,
-        engines=[engine.name for engine in engines],
-        supported_languages=engine.get_supported_languages(),
-        use_text_fields=use_text_fields,
-        could_not_switch_languages=could_not_switch_languages,
-    ))
+    response = await make_response(
+        await render_template(
+            "index.html",
+            inp=inp,
+            translation=translation,
+            from_l=from_lang,
+            from_l_code=from_l_code,
+            tts_from=tts_from,
+            tts_to=tts_to,
+            to_l=to_lang,
+            to_l_code=to_l_code,
+            engine=engine.name,
+            engines=[engine.name for engine in engines],
+            supported_languages=engine.get_supported_languages(),
+            use_text_fields=use_text_fields,
+            could_not_switch_languages=could_not_switch_languages,
+        )
+    )
 
     if request.method == "POST":
-        response.set_cookie('from_lang', to_lang_code(from_lang, engine))
-        response.set_cookie('to_lang', to_lang_code(to_lang, engine))
+        response.set_cookie("from_lang", to_lang_code(from_lang, engine))
+        response.set_cookie("to_lang", to_lang_code(to_lang, engine))
 
     return response
 
@@ -270,4 +273,4 @@ async def about():
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0')
+    app.run(port=5000, host="0.0.0.0")
