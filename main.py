52d0fc332f4d55a87a800c978f18a90c47642aca
fattalion <fattalion@disroot.org>
2021-10-04 17:42:57 +0300

    Enable `strict_slashes` globally
    The other changes are from `black`.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from quart import Quart, render_template, request, redirect, make_response, send_file, Response
+from quart import (
+    Quart,
+    render_template,
+    request,
+    redirect,
+    make_response,
+    send_file,
+    Response,
+)
 from configparser import ConfigParser
 from urllib.parse import urlencode
 from wsgiref.util import FileWrapper
@@ -40,10 +48,11 @@ if not engines:
 
 app = Quart(__name__)
 
+app.url_map.strict_slashes = False
+
 # NOTE: Legacy Endpoint. Use "/api"
 @app.route(
     "/translate/<string:from_language>/<string:to_language>/<string:input_text>/",
-    strict_slashes=False
 )
 async def translate(from_language, to_language, input_text):
     return engines[0].translate(
@@ -51,7 +60,7 @@ async def translate(from_language, to_language, input_text):
     )
 
 
-@app.route("/api/translate/", strict_slashes=False)
+@app.route("/api/translate/")
 async def api_translate():
     engine_name = request.args.get("engine")
     text = request.args.get("text")
@@ -66,7 +75,7 @@ async def api_translate():
     return engine.translate(text, from_language=from_language, to_language=to_language)
 
 
-@app.route("/api/get_languages/", strict_slashes=False)
+@app.route("/api/get_languages/")
 async def api_get_languages():
     engine_name = request.args.get("engine")
     engine = get_engine(engine_name, engines, engines[0])
@@ -74,7 +83,7 @@ async def api_get_languages():
     return engine.get_languages()
 
 
-@app.route("/api/tts/", strict_slashes=False)
+@app.route("/api/tts/")
 async def api_tts():
     engine_name = request.args.get("engine")
     text = request.args.get("text")
@@ -89,16 +98,18 @@ async def api_tts():
     USER_AGENT = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
 
     if url != None:
-        b = BytesIO(requests.get(
-            url, headers={"Referrer": None, "User-Agent": USER_AGENT}
-        ).content)
+        b = BytesIO(
+            requests.get(
+                url, headers={"Referrer": None, "User-Agent": USER_AGENT}
+            ).content
+        )
         w = FileWrapper(b)
         return Response(w, mimetype="audio/mpeg")
 
     abort(404)
 
 
-@app.route("/switchlanguages/", methods=["POST"], strict_slashes=False)
+@app.route("/switchlanguages/", methods=["POST"])
 async def switchlanguages():
     form = await request.form
 
@@ -153,7 +164,7 @@ async def switchlanguages():
     return response
 
 
-@app.route("/typingiscool/", methods=["POST"], strict_slashes=False)
+@app.route("/typingiscool/", methods=["POST"])
 async def typingiscool():
     form = await request.form
 
@@ -182,7 +193,7 @@ async def typingiscool():
     )
 
 
-@app.route("/", methods=["GET", "POST"], strict_slashes=False)
+@app.route("/", methods=["GET", "POST"])
 async def index():
     engine_name = request.args.get("engine")
 
@@ -271,7 +282,7 @@ async def index():
     return response
 
 
-@app.route("/about", methods=["GET"], strict_slashes=False)
+@app.route("/about", methods=["GET"])
 async def about():
     return await render_template("about.html")
 
